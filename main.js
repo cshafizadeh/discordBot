e4943cb9d60c01aca85bbee1c0571ce4b703142f
jonathandow <dow3@purdue.edu>
2022-07-26 21:24:58 -0700

    Fixed main

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord.js');
+const { Routes, Client, GatewayIntentBits } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
 
 const commands = [];
