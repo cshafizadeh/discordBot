816980199fefc82756f73fc30470483462771d13
jonathandow <dow3@purdue.edu>
2022-07-26 20:40:24 -0700

    Fixed Issue

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ client.on('interactionCreate', async interaction => {
 	if (commandName === 'ping') {
 		await interaction.reply('Pong!');
 	} else if (commandName === 'server') {
-		await interaction.reply('Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nThis Server was Created On: ${interaction.guild.createdAt}');
+		await interaction.reply("Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nThis Server was Created On: ${interaction.guild.createdAt}");
 	} else if (commandName === 'user') {
-		await interaction.reply('Your tag: ${interaction.user.tag}\nYour ID: ${interaction.user.id}');
+		await interaction.reply("Your tag: ${interaction.user.tag}\nYour ID: ${interaction.user.id}");
 	}
 });
 
