03471ccc317bd6db51613bab2712eb6ce234e11a
cshafizadeh <shafizac@oregonstate.edu>
2022-07-26 21:36:03 -0700

    updated server.js and user.js to backticks  */

--- commands/server.js
+++ commands/server.js
@@ -5,6 +5,6 @@ module.exports = {
 		.setName('server')
 		.setDescription('Replies with Server Info!'),
 	async execute(interaction) {
-		await interaction.reply("Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nThis Server was Created On: ${interaction.guild.createdAt}");
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nThis Server was Created On: ${interaction.guild.createdAt}`);
 	},
 };
