a38d21034d3ca7bc7ab126f9593fa957ca63c145
jonathandow <dow3@purdue.edu>
2022-07-26 20:21:20 -0700

    Added Commands.

--- main.js
+++ main.js
@@ -8,5 +8,19 @@ client.once('ready', () => {
 	console.log('Code Bot is Up and Running!');
 });
 
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nThis Server was Created On: ${interaction.guild.createdAt}');
+	} else if (commandName === 'user') {
+		await interaction.reply('Your tag: ${interaction.user.tag}\nYour ID: ${interaction.user.id}');
+	}
+});
+
 client.login(token);
 
