df3e82ebf857fd78b5712b0026e0ffcf6d90b094
cshafizadeh <shafizac@oregonstate.edu>
2022-07-26 21:32:22 -0700

    updated main.js

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 const fs = require('node:fs');
 const path = require('node:path');
-const { REST } = require('@discordjs/rest');
-const { Routes, Client, GatewayIntentBits } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
 
-const commands = [];
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
+// Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
- client.once('ready', () => {
- 	console.log('Code Bot is Up and Running!');
- });
+client.commands = new Collection();
 
-client.login(token);
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
-	commands.push(command.data.toJSON());
+	client.commands.set(command.data.name, command);
 }
 
-const rest = new REST({ version: '10' }).setToken(token);
+client.once('ready', () => {
+	console.log('Code Bot is Up and Running!');
+});
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+client.login(token);
