054463ac642d7da3ca01bd6761211c8158eb836f
cshafizadeh <shafizac@oregonstate.edu>
2022-07-26 20:45:45 -0700

    updated main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once('ready', () => {
+	console.log('Code Bot is Up and Running!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nThis Server was Created On: ${interaction.guild.createdAt}`);
+	} else if (commandName === 'user') {
+		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour ID: ${interaction.user.id}`);
+	} else if (commandName === 'noah') {
+        await interaction.reply('loves man nuts');
+    }
+});
+
+client.login(token);
